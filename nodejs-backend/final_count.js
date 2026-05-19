8da88dd0c86418c54cf43dc88f8a47da3753f4a2
zainulabdin <zainalikhan338@gmail.com>
2026-05-19 20:31:42 +0500

    chore: Clean up 60+ bogus test scripts, logs, and compiled files from backend

--- nodejs-backend/final_count.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-
-async function count() {
-  const c = await prisma.attendanceRecord.count();
-  console.log('Total Attendance Records in DB:', c);
-}
-
-count().catch(console.error).finally(() => prisma.$disconnect());
